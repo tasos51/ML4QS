5defcab156f2c778d5c8b1bf361ecd0e6fbdaa3e
Tasos <tasos1109@gmail.com>
2021-06-05 17:09:37 +0200

    ch2 adding labels

--- Python3Code/crowdsignals_ch2.py
+++ Python3Code/crowdsignals_ch2.py
@@ -24,7 +24,7 @@ RESULT_FNAME = 'our_set_result.csv'
 
 # Set a granularity (the discrete step size of our time series data). We'll use a course-grained granularity of one
 # instance per minute, and a fine-grained one with four instances per second.
-GRANULARITIES = [60000, 250]
+GRANULARITIES = [60000, 120000]
 
 # We can call Path.mkdir(exist_ok=True) to make any required directories if they don't already exist.
 [path.mkdir(exist_ok=True, parents=True) for path in [DATASET_PATH, RESULT_PATH]]
@@ -78,9 +78,9 @@ for milliseconds_per_instance in GRANULARITIES:
     DataViz.plot_dataset_boxplot(dataset, ['acc_phone_x','acc_phone_y','acc_phone_z'])
 
     # Plot all data
-    DataViz.plot_dataset(dataset, ['acc_', 'light_', 'prox'],
-                                  ['like', 'like', 'like'],
-                                  ['line', 'line', 'line'])
+    DataViz.plot_dataset(dataset, ['acc_', 'light_', 'prox', 'label'],
+                                  ['like', 'like', 'like', 'like'],
+                                  ['line', 'line', 'line', 'points'])
 
     # And print a summary of the dataset.
     util.print_statistics(dataset)
